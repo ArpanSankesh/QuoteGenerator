b0b76306632e45d8eea34f6064c59f8f6829a37b
ArpanSankesh <arpansankesh830@gmail.com>
2024-01-02 16:01:39 +0530

    uploding projects code HTML AND CSS

---
+++ script.js
@@ -0,0 +1,6 @@
+const quote = document.querySelector('.quote');
+const author = document.querySelector('.author-name');
+const generate = document.querySelector('.generate');
+const speach = document.querySelector('.speach');
+const copy = document.querySelector('.copy');
+const twiter = document.querySelector('.twiter');
