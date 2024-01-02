b3481648e585e99ec7848293fae997c8eccbdf7b
ArpanSankesh <arpansankesh830@gmail.com>
2024-01-02 20:33:09 +0530

    speech and copy added

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
 const quote = document.querySelector(".quote");
 const author = document.querySelector(".author-name");
-const generate = document.querySelector(".generate");
-const speach = document.querySelector(".speach");
-const copy = document.querySelector(".copy");
-const twiter = document.querySelector(".twiter");
+const generateBtn = document.querySelector(".generate");
+const speakBtn = document.querySelector(".speach");
+const copyBtn = document.querySelector(".copy");
+const twiterBTn = document.querySelector(".twiter");
+
+let speech = null;
 
 const generateQuote = function () {
+    copyBtn.classList.remove('copyBtn')
+    generateBtn.classList.add('loading')
+    generateBtn.innerHTML = "Loading...";
   fetch("https://api.quotable.io/random")
     .then((res) => res.json())
     .then((result) => {
       console.log(result);
       quote.innerHTML = `<i class="fa-solid fa-quote-left"></i> ${result.content} <i class="fa-solid fa-quote-right"></i>`;
       author.innerHTML = `-${result.author}`;
+      generateBtn.classList.remove('loading')
+      generateBtn.innerHTML = "NEW QUOTE";
 
-      localStorage.setItem("lastQuote", JSON.stringify(result));
+
+    //   localStorage.setItem("lastQuote", JSON.stringify(result));
     });
 };
 
-document.addEventListener("DOMCOnthentLoaded", generateQuote);
+// document.addEventListener("DOMCOnthentLoaded", generateQuote);
 
-generate.addEventListener("click", generateQuote);
+speakBtn.addEventListener('click', function(){
+    if (speech ) {
+        speechSynthesis.cancel(); 
+        return;
+    }
+    speech = new SpeechSynthesisUtterance(`${quote.innerText} by ${author.innerText}`);
+    speechSynthesis.speak(speech);
+});
+
+copyBtn.addEventListener('click', function(){
+    navigator.clipboard.writeText(`"${quote.innerText}"`);
+    copyBtn.classList.add('copyBtn');
+});
+
+generateBtn.addEventListener("click", generateQuote);
